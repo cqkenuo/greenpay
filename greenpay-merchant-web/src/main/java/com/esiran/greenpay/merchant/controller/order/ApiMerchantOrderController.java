f9732d1f612b8a25eeee74b9d874ac9d28dba5d7
GuHaiTao <1021349457@qq.com>
2020-04-28 16:41:24 +0800

    admin merchant 分离

--- greenpay-merchant-web/src/main/java/com/esiran/greenpay/merchant/controller/order/ApiMerchantOrderController.java
+++ greenpay-merchant-web/src/main/java/com/esiran/greenpay/merchant/controller/order/ApiMerchantOrderController.java
@@ -3,9 +3,13 @@ package com.esiran.greenpay.merchant.controller.order;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 
+import com.esiran.greenpay.common.exception.PostResourceException;
+import com.esiran.greenpay.common.exception.ResourceNotFoundException;
 import com.esiran.greenpay.pay.entity.*;
 import com.esiran.greenpay.pay.service.*;
-import org.springframework.ui.ModelMap;
+import com.esiran.greenpay.settle.entity.SettleOrderInputDTO;
+import com.esiran.greenpay.settle.service.ISettleOrderService;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
@@ -18,11 +22,13 @@ public class ApiMerchantOrderController {
 
     private final IOrderService payOrderService;
     private final IExtractService extractService;
+    private final ISettleOrderService settleOrderService;
 
 
-    public ApiMerchantOrderController(IOrderService payOrderService, IExtractService extractService) {
+    public ApiMerchantOrderController(IOrderService payOrderService, IExtractService extractService, ISettleOrderService settleOrderService) {
         this.payOrderService = payOrderService;
         this.extractService = extractService;
+        this.settleOrderService = settleOrderService;
     }
 
     @GetMapping("/orders")
@@ -35,7 +41,8 @@ public class ApiMerchantOrderController {
     }
 
     @PostMapping("/payextract")
-    public Map payExtract(){
+    public Map payExtract(@RequestBody @Validated SettleOrderInputDTO settleOrderInputDTO) throws ResourceNotFoundException, PostResourceException {
+        settleOrderService.postOrder(settleOrderInputDTO);
         Map m = new HashMap<String,String>();
         m.put("code","1");
         m.put("msg","提交成功");

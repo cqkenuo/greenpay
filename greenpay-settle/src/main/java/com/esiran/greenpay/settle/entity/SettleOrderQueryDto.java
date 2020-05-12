915fc23480c254b26116733dec2183b2837540c6
han <15086978647@qq.com>
2020-05-12 18:33:37 +0800

    添加搜索功能

---
+++ greenpay-settle/src/main/java/com/esiran/greenpay/settle/entity/SettleOrderQueryDto.java
@@ -0,0 +1,9 @@
+package com.esiran.greenpay.settle.entity;
+
+/**
+ * @author han
+ * @Package com.esiran.greenpay.settle.entity
+ * @date 2020/5/12 16:52
+ */
+public class SettleOrderQueryDto {
+}

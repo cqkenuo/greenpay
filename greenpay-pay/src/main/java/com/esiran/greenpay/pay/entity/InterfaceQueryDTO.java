e6408706af1b5541d4b17c7454833bf7fa91a9a8
han <15086978647@qq.com>
2020-05-13 19:27:49 +0800

    1、添加支付配置搜索。2、添加商户删除

---
+++ greenpay-pay/src/main/java/com/esiran/greenpay/pay/entity/InterfaceQueryDTO.java
@@ -0,0 +1,13 @@
+package com.esiran.greenpay.pay.entity;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class InterfaceQueryDTO {
+    private Integer id;
+    private String interfaceCode;
+    private String interfaceName;
+
+}

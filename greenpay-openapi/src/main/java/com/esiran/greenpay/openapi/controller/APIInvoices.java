1ba793c7bab19301ff65870c84ceeb32198d9e76
admin <admin.git@ishiyou.net>
2020-05-14 20:45:10 +0800

    update

--- greenpay-openapi/src/main/java/com/esiran/greenpay/openapi/controller/APIInvoices.java
+++ greenpay-openapi/src/main/java/com/esiran/greenpay/openapi/controller/APIInvoices.java
@@ -1,9 +1,11 @@
 package com.esiran.greenpay.openapi.controller;
 
 import com.esiran.greenpay.common.entity.APIException;
+import com.esiran.greenpay.common.exception.ResourceNotFoundException;
 import com.esiran.greenpay.merchant.entity.Merchant;
 import com.esiran.greenpay.openapi.entity.Invoice;
 import com.esiran.greenpay.openapi.entity.InvoiceInputDTO;
+import com.esiran.greenpay.openapi.entity.InvoiceQueryInputDTO;
 import com.esiran.greenpay.openapi.security.OpenAPISecurityUtils;
 import com.esiran.greenpay.openapi.service.IInvoiceService;
 import org.modelmapper.ModelMapper;
@@ -22,7 +24,19 @@ public class APIInvoices {
     public APIInvoices(IInvoiceService invoiceService) {
         this.invoiceService = invoiceService;
     }
-
+    @GetMapping
+    public Invoice get(@Valid InvoiceQueryInputDTO queryInputDTO) throws APIException {
+        try {
+            return invoiceService.getOneByQuery(queryInputDTO);
+        } catch (Exception e) {
+            if (e instanceof IllegalArgumentException){
+                throw new APIException(e.getMessage(),"ARGUMENT_NOT_VALID", 400);
+            }else if(e instanceof ResourceNotFoundException){
+                throw new APIException(e.getMessage(),"RESOURCE_NOT_FOUND", 404);
+            }
+            throw new APIException(e.getMessage(),"SYSTEM_ERROR", 500);
+        }
+    }
     @PostMapping
     public Invoice create(@Valid InvoiceInputDTO invoiceDto) throws Exception {
         Merchant merchant = OpenAPISecurityUtils.getSubject();
--- greenpay-openapi/src/main/java/com/esiran/greenpay/openapi/service/IInvoiceService.java
+++ greenpay-openapi/src/main/java/com/esiran/greenpay/openapi/service/IInvoiceService.java
@@ -2,9 +2,11 @@ package com.esiran.greenpay.openapi.service;
 
 import com.esiran.greenpay.common.entity.APIException;
 import com.esiran.greenpay.common.exception.PostResourceException;
+import com.esiran.greenpay.common.exception.ResourceNotFoundException;
 import com.esiran.greenpay.merchant.entity.Merchant;
 import com.esiran.greenpay.openapi.entity.Invoice;
 import com.esiran.greenpay.openapi.entity.InvoiceInputDTO;
+import com.esiran.greenpay.openapi.entity.InvoiceQueryInputDTO;
 import com.esiran.greenpay.pay.entity.PayOrder;
 
 import javax.servlet.http.HttpServletRequest;
@@ -14,4 +16,5 @@ public interface IInvoiceService {
     Invoice createInvoiceByInput(InvoiceInputDTO invoiceInputDTO, Merchant merchant) throws Exception;
     PayOrder createPretreatmentInvoice(InvoiceInputDTO invoiceInputDTO, Merchant merchant) throws APIException;
     String handleInvoiceCallback(HttpServletRequest request, HttpServletResponse response, String orderNo);
+    Invoice getOneByQuery(InvoiceQueryInputDTO queryInputDTO) throws ResourceNotFoundException;
 }

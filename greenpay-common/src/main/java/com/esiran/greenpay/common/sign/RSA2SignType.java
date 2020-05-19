34bcc062eb88117bc5047937a5d96d2544f24daf
admin <admin.git@ishiyou.net>
2020-05-19 17:29:14 +0800

    update

--- greenpay-common/src/main/java/com/esiran/greenpay/common/sign/RSA2SignType.java
+++ greenpay-common/src/main/java/com/esiran/greenpay/common/sign/RSA2SignType.java
@@ -1,10 +1,13 @@
 package com.esiran.greenpay.common.sign;
 
 import com.esiran.greenpay.common.util.RSAUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.nio.charset.StandardCharsets;
 
 public class RSA2SignType extends SignTypeAbs {
+    private static final Logger logger = LoggerFactory.getLogger(RSA2SignType.class);
     public RSA2SignType(String principal) {
         super(principal);
     }
@@ -18,6 +21,8 @@ public class RSA2SignType extends SignTypeAbs {
     public String sign2(String credential) {
         if (getPrincipal() == null || getPrincipal().length() == 0) return null;
         byte[] principalBytes = getPrincipal().getBytes(StandardCharsets.UTF_8);
-        return RSAUtil.sign(principalBytes,credential);
+        String sign = RSAUtil.sign(principalBytes,credential);
+        logger.debug("Sign With Rsa2: {}", sign);
+        return sign;
     }
 }
